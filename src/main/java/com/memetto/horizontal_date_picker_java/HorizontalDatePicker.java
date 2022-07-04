a718c2a735558029bb89426fa1695874aeb53ebb
memettok <98664649+memettok@users.noreply.github.com>
2022-07-04 01:21:34 -0400

    HorizontalDatePicker new

--- src/main/java/com/memetto/horizontal_date_picker_java/HorizontalDatePicker.java
+++ src/main/java/com/memetto/horizontal_date_picker_java/HorizontalDatePicker.java
@@ -1,5 +1,9 @@
 package com.memetto.horizontal_date_picker_java;
 
 public class HorizontalDatePicker {
+    String title;
 
+    public HorizontalDatePicker(String title) {
+        this.title = title;
+    }
 }
